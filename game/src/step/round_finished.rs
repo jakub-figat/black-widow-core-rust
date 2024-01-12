226c1f94cc4f9372549fbed7a7f4eb0dd0e3f5bd
jakub-figat <jakub.figat@neontri.com>
2024-01-12 14:27:23 +0100

    Add tests for round in progress step, fix disptach payload bug

--- game/src/step/round_finished.rs
+++ game/src/step/round_finished.rs
@@ -1,7 +1,45 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
+use crate::error::{GameError, GameResult};
+use crate::step::card_exchange::CardExchangeState;
+use crate::step::GameStep;
+
+
+impl GameStep<RoundFinishedState> {
+    pub fn claim_readiness(&mut self, player: &str) -> GameResult<()> {
+        if self.state.players_ready.contains(player) {
+            Err(
+                GameError::InvalidAction(
+                    format!("Player {} has already claimed readiness", player)
+                )
+            )?
+        }
+
+        self.state.players_ready.insert(player.to_string());
+        Ok(())
+    }
+
+    pub fn should_switch(&self) -> bool {
+        self.state.players_ready.len() == self.players.len()
+    }
+
+    pub fn game_finished(&self) -> bool {
+        self.scores.iter().max_by_key(|(_, &score)| score).unwrap().1.clone() >= 100
+    }
+
+    pub fn to_card_exchange(self) -> GameStep<CardExchangeState> {
+        GameStep {
+            players: self.players,
+            player_to_player_map: self.player_to_player_map,
+            scores: self.scores,
+            player_decks: self.player_decks,
+            state: CardExchangeState::new()
+        }
+    }
+
+}
 
 pub struct RoundFinishedState {
-    users_ready: HashMap<String, bool>
+    pub players_ready: HashSet<String>
 }
 
 impl RoundFinishedState {}
--- game/src/step/round_in_progress.rs
+++ game/src/step/round_in_progress.rs
@@ -1,9 +1,10 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use crate::card::{Card, CardSuit};
 use crate::card::CardSuit::Heart;
 use crate::error::{GameError, GameResult};
 use crate::payload::PlaceCardPayload;
 use crate::step::GameStep;
+use crate::step::round_finished::RoundFinishedState;
 
 impl GameStep<RoundInProgressState> {
     pub fn validate_payload(&self, payload: &PlaceCardPayload) -> GameResult<()> {
@@ -63,14 +64,11 @@ impl GameStep<RoundInProgressState> {
             self.state.table_suit = Some(payload.card.suit);
         }
 
-        // if everyone has placed a card, calculate who gets the score assigned and pick him as next player
-        if self.state.cards_on_table.len() == 3 {
-            let scoring_player = self.get_scoring_player();
-            let score = self.get_total_score_of_cards_on_table();
-            *self.scores.entry(scoring_player.clone()).or_insert(0) += score;
+        if self.state.cards_on_table.len() == self.players.len() {
+            self.prepare_table_for_next_turn();
+        } else {
+            self.state.current_player = self.player_to_player_map[&self.state.current_player].clone();
         }
-
-        self.prepare_table_for_next_turn();
     }
 
     fn place_card(&mut self, card: &Card) {
@@ -95,19 +93,30 @@ impl GameStep<RoundInProgressState> {
     }
 
     fn prepare_table_for_next_turn(&mut self) {
+        let scoring_player = self.get_scoring_player();
+        let score = self.get_total_score_of_cards_on_table();
+
+        *self.scores.entry(scoring_player.clone()).or_insert(0) += score;
+        self.state.current_player = scoring_player;
+
         self.state.cards_on_table = HashMap::new();
         self.state.table_suit = None;
-        self.state.current_player = self.player_to_player_map[&self.state.current_player].clone();
     }
 
-    // TODO: implement those and write tests
-    // pub fn should_switch(&self) -> bool {
-    //
-    // }
-    
-    // pub fn to_round_finished(self) -> RoundFinishedState {
-    //
-    // }
+    pub fn should_switch(&self) -> bool {
+        self.player_decks.iter()
+            .all(|(_, cards)| cards.is_empty())
+    }
+
+    pub fn to_round_finished(self) -> GameStep<RoundFinishedState> {
+        GameStep {
+            players: self.players,
+            player_to_player_map: self.player_to_player_map,
+            scores: self.scores,
+            player_decks: self.player_decks,
+            state: RoundFinishedState { players_ready: HashSet::new()}
+        }
+    }
 }
 
 
@@ -116,4 +125,153 @@ pub struct RoundInProgressState {
     pub current_player: String,
     pub table_suit: Option<CardSuit>,
     pub cards_on_table: HashMap<String, Card>
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::card::CardSuit::{Diamond, Spade};
+    use crate::helper::get_player_to_player_map;
+    use super::*;
+
+    fn get_players() -> Vec<String> {
+        vec![
+            "1".to_string(),
+            "2".to_string(),
+            "3".to_string()
+        ]
+    }
+
+    fn get_step_from_players(players: &Vec<String>) -> GameStep<RoundInProgressState> {
+        GameStep {
+            players: players.clone(),
+            player_to_player_map: get_player_to_player_map(&players),
+            scores: HashMap::new(),
+            player_decks: HashMap::from_iter(
+                players.iter()
+                    .cloned()
+                    .map(|player| (player, HashSet::new()))
+                    .collect::<HashMap<_, _>>()
+            ),
+            state: RoundInProgressState {
+                current_player: players[0].clone(),
+                table_suit: None,
+                cards_on_table: HashMap::new()
+            }
+        }
+    }
+
+    #[test]
+    fn validate_payload_when_player_places_matching_suit() {
+        let players = get_players();
+        let mut step = get_step_from_players(&players);
+        step.state.table_suit = Some(Spade);
+
+        let card = Card::new(Spade, 2);
+        step.player_decks.get_mut(&players[0]).unwrap().insert(card.clone());
+        let payload = PlaceCardPayload {card};
+
+        assert!(step.validate_payload(&payload).is_ok());
+    }
+
+    #[test]
+    fn validate_payload_when_places_places_mismatched_suit_despite_having_matching_suit() {
+        let players = get_players();
+        let mut step = get_step_from_players(&players);
+        step.state.table_suit = Some(Spade);
+
+        let card = Card::new(Diamond, 2);
+        let player_deck = step.player_decks.get_mut(&players[0]).unwrap();
+        player_deck.insert(card.clone());
+        player_deck.insert(Card::new(Spade, 2));
+
+        let payload = PlaceCardPayload {card};
+
+        let expected_error = Err(
+            GameError::InvalidAction(
+                "Player 1 tried to place DIAMOND, despite having SPADE in deck".to_string()
+            )
+        );
+        assert_eq!(step.validate_payload(&payload), expected_error);
+    }
+
+    #[test]
+    fn validate_payload_when_places_places_mismatched_suit_and_does_not_have_matching_suit() {
+        let players = get_players();
+        let mut step = get_step_from_players(&players);
+        step.state.table_suit = Some(Spade);
+
+        let card = Card::new(Diamond, 2);
+        step.player_decks.get_mut(&players[0]).unwrap().insert(card.clone());
+
+        let payload = PlaceCardPayload {card};
+        assert!(step.validate_payload(&payload).is_ok());
+    }
+
+    #[test]
+    fn validate_payload_when_table_suit_is_none_and_player_places_heart_despite_having_other_suits() {
+        let players = get_players();
+        let mut step = get_step_from_players(&players);
+        step.player_decks.get_mut(&players[0]).unwrap().insert(Card::new(Spade, 2));
+        let card = Card::new(Heart, 2);
+
+        step.player_decks.get_mut(&players[0]).unwrap().insert(card.clone());
+        let payload = PlaceCardPayload {card};
+
+        let expected_error = Err(
+            GameError::InvalidAction(
+                "Player 1 tried to place Heart suit on the table, despite having other suits left".to_string()
+            )
+        );
+        assert_eq!(step.validate_payload(&payload), expected_error);
+    }
+
+    #[test]
+    fn validate_payload_when_table_suit_is_none_and_heart_is_placed_and_its_player_only_card() {
+        let players = get_players();
+        let mut step = get_step_from_players(&players);
+
+        let card = Card::new(Heart, 2);
+        step.player_decks.get_mut(&players[0]).unwrap().insert(card.clone());
+        let payload = PlaceCardPayload {card};
+
+        assert!(step.validate_payload(&payload).is_ok());
+    }
+
+    #[test]
+    fn validate_payload_when_table_suit_is_none_and_non_heart_is_placed() {
+        let players = get_players();
+        let mut step = get_step_from_players(&players);
+
+        let card = Card::new(Spade, 2);
+        step.player_decks.get_mut(&players[0]).unwrap().insert(card.clone());
+        let payload = PlaceCardPayload {card};
+
+        assert!(step.validate_payload(&payload).is_ok());
+    }
+
+    #[test]
+    fn dispatch_payload_when_decks_are_left_empty() {
+        let players = get_players();
+        let mut step = get_step_from_players(&players);
+        let (card_1, card_2, card_3) = (
+            Card::new(Spade, 5), Card::new(Spade, 12), Card::new(Spade, 4)
+        );
+        step.player_decks = HashMap::from(
+            [
+                ("1".to_string(), HashSet::from([card_1.clone()])),
+                ("2".to_string(), HashSet::from([card_2.clone()])),
+                ("3".to_string(), HashSet::from([card_3.clone()])),
+            ]
+        );
+
+        step.dispatch_payload(&PlaceCardPayload {card: card_1});
+        step.dispatch_payload(&PlaceCardPayload {card: card_2});
+        step.dispatch_payload(&PlaceCardPayload {card: card_3});
+
+        assert_eq!(&step.state.current_player, "2");
+        assert_eq!(step.scores, HashMap::from([("2".to_string(), 13)]));
+        assert!(step.state.cards_on_table.is_empty());
+        assert!(step.state.table_suit.is_none());
+        assert!(step.should_switch());
+    }
 }
